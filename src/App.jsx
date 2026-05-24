0aa75cc969d8088970df133874d0d0209e4f77d7
heranp27 <heranpatel27@gmail.com>
2026-05-24 16:26:55 -0500

    Started subway scene redesign

--- src/App.jsx
+++ src/App.jsx
@@ -7,72 +7,59 @@ import Contact from "./pages/Contact"
 
 function Home() {
   const playClick = () => {
-  const audio = new Audio("/sounds/click.mp3")
-  audio.play()
-}
+    const audio = new Audio("/sounds/click.mp3")
+    audio.play()
+  }
+
   return (
     <motion.div
       className="container"
       initial={{ opacity: 0, x: -100 }}
       animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 100 }}
       transition={{ duration: 0.8 }}
     >
-      <div className="red-slash slash-one"></div>
-      <div className="red-slash slash-two"></div>
-      <div className="white-slash"></div>
-      <div className="floating-circle circle-one"></div>
-      <div className="floating-circle circle-two"></div>
-      <div className="floating-circle circle-three"></div>
+      <div className="station-scene">
+        <div className="back-wall"></div>
+        <div className="ceiling-lights"></div>
+        <div className="far-tunnel"></div>
+
+        <div className="train train-left"></div>
+        <div className="train train-right"></div>
+
+        <div className="character char-one"></div>
+        <div className="character char-two"></div>
+        <div className="character char-three"></div>
+      </div>
+
       <div className="title-plate">
         <h1>HERAN PATEL</h1>
       </div>
 
-      <p className="subtitle">
-        Persona 5 Inspired Portfolio
-      </p>
+      <p className="subtitle">Persona 5 Inspired Portfolio</p>
 
       <div className="menu">
         <Link to="/about">
-          <motion.button
-            className="menu-button"
-            whileHover={{ scale: 1.1, x: 10 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={playClick}
-          >
-            ABOUT
+          <motion.button className="menu-button" whileHover={{ scale: 1.1, x: 10 }} whileTap={{ scale: 0.95 }} onClick={playClick}>
+            PERSONAL INFO
           </motion.button>
         </Link>
 
         <Link to="/projects">
-          <motion.button
-            className="menu-button"
-            whileHover={{ scale: 1.1, x: 10 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={playClick}
-          >
+          <motion.button className="menu-button" whileHover={{ scale: 1.1, x: 10 }} whileTap={{ scale: 0.95 }} onClick={playClick}>
             PROJECTS
           </motion.button>
         </Link>
 
         <Link to="/skills">
-          <motion.button
-            className="menu-button"
-            whileHover={{ scale: 1.1, x: 10 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={playClick}
-          >
+          <motion.button className="menu-button" whileHover={{ scale: 1.1, x: 10 }} whileTap={{ scale: 0.95 }} onClick={playClick}>
             SKILLS
           </motion.button>
         </Link>
 
         <Link to="/contact">
-          <motion.button
-            className="menu-button"
-            whileHover={{ scale: 1.1, x: 10 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={playClick}
-          >
-            CONTACT
+          <motion.button className="menu-button" whileHover={{ scale: 1.1, x: 10 }} whileTap={{ scale: 0.95 }} onClick={playClick}>
+            SOCIALS
           </motion.button>
         </Link>
       </div>
